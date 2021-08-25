be8eb3c2bd36d6ca76e9648cf69c36b824bf2d1e
Betty <bear840505@yahoo.com.tw>
2021-08-25 17:01:18 +0800

    second commit

--- src/js/main.js
+++ src/js/main.js
@@ -53,13 +53,100 @@ $(function() {
   }
 })
 
+$(function() {
+  let balls = []
+  let gradients = [
+    'radial-gradient(circle at 50% 50%, rgba(255, 249, 212, 0.5), rgba(255, 251, 227, 0) 61%)',
+    'radial-gradient(circle at 50% 50%, rgba(255, 249, 212, 0.5), rgba(255, 251, 227, 0) 75%)',
+    'radial-gradient(circle at 50% 50%, rgba(250, 241, 185, 0.5), rgba(255, 251, 227, 0) 70%)'
+  ]
+  let Ball = function(el, options) {
+    this.setting = {
+      minSpeed: null,
+      maxSpeed: null
+    }
+    this.setting = $.extend(this.settings, options)
+    this.el = el
+    this.div = document.createElement('div')
+    this.div.className = 'moving-ball';
+    this.randomWidth = this.random(400, 800)
+    this.div.style.width = this.randomWidth + 'px'
+    this.div.style.height = this.randomWidth + 'px'
+    this.div.style.backgroundImage = gradients[Math.floor(Math.random() * gradients.length)]
+    this.speedX = this.random(this.setting.minSpeed, this.setting.maxSpeed);
+    this.speedY = this.random(this.setting.minSpeed, this.setting.maxSpeed);
+    this.x = this.random(0, $(window).innerWidth())
+    this.y = this.random(0, this.el.height())
+  }
+  Ball.prototype.random = function (min, max) {
+    return Math.random() * (max - min) + min
+  }
+  Ball.prototype.draw = function () {
+    this.div.style.left = this.x + 'px'
+    this.div.style.top = this.y + 'px'
+  }
+  Ball.prototype.update = function () {
+    this.x += this.speedX
+    this.y += this.speedY
+    if (this.x < 0 - this.randomWidth/2) {
+      this.x = 0 - this.randomWidth/2;
+      this.speedX *= -1
+    }
+    if (this.y < 0 - this.randomWidth/2) {
+      this.y = 0 - this.randomWidth/2;
+      this.speedY *= -1
+    }
+    if (this.x > $(window).innerWidth() - this.randomWidth/2) {
+      this.x = $(window).innerWidth() - this.randomWidth/2
+      this.speedX *= -1
+    }
+    if (this.y > this.el.height() - this.randomWidth/2) {
+      this.y = this.el.height() - this.randomWidth/2
+      this.speedY *= -1
+    }
+  }
+  let BallMoving = function(el, options) {
+    this.setting = {
+      num: 4,
+      minSpeed: 2,
+      maxSpeed: 6
+    }
+    this.el = el
+    this.setting = $.extend(this.settings, options)
+    this.createBall()
+    this.start()
+  }
+  BallMoving.prototype.createBall = function() {
+    for(let i = 0; i< this.setting.num; i++) {
+      let ball = new Ball(this.el, {
+        minSpeed: this.setting.minSpeed,
+        maxSpeed: this.setting.maxSpeed
+      });
+      this.el.append(ball.div);
+      balls.push(ball);
+    }
+  }
+  BallMoving.prototype.start = function() {
+    for (let i = 0; i < balls.length; i++) {
+      balls[i].draw()
+      balls[i].update()
+    }
+    requestAnimationFrame(this.start.bind(this))
+  }
+  new BallMoving($('header#header'), {
+    num: 8,
+    minSpeed: 2,
+    maxSpeed: 6
+  })
+})
+
 // 滾動至 about
 $(function() {
   $(window).scroll(function() {
-    if ($('.l-about__bg')[0] !== undefined) {
-      let $about = $('.l-about__bg')
+    if ($('.l-about')[0] !== undefined) {
+      let $about = $('.l-about')
       let aboutTop = $about.offset().top - 300
-      let aboutBottom = $about.offset().top + $about.outerHeight()
+      let aboutBottom = $about.offset().top + $about.outerHeight() / 2
       if (this.scrollY > aboutTop && this.scrollY < aboutBottom) {
         $('body').addClass('is-about-active')
       } else {
@@ -74,7 +161,7 @@ $(function() {
   $(window).scroll(function() {
     if ($('.l-contact')[0] !== undefined) {
       let $contact = $('.l-contact')
-      let contactTop = $contact.offset().top - 30
+      let contactTop = $contact.offset().top - 300
       let contactBottom = $contact.offset().top + $contact.outerHeight() / 2
       if (this.scrollY > contactTop && this.scrollY < contactBottom) {
         $('body').addClass('is-contact-active')
@@ -110,26 +197,22 @@ $(function() {
     let newsSwiper = new Swiper('.l-popular__swiper', {
       freeMode : true,
       loop: true,
-      speed: 2000,
+      loopslides: 20,
+      speed: 5000,
       slidesPerView : 1.5,
       autoplay: {
         delay: 0,
-        stopOnLastSlide: false,
         disableOnInteraction: false,
       },
-      spaceBetween: 30,
       breakpoints: {
         992: {
           slidesPerView: 4,
-          spaceBetween: 30
         },
         768: {
           slidesPerView: 3,
-          spaceBetween: 30
         },
         576: {
-          slidesPerView: 2,
-          spaceBetween: 30
+          slidesPerView: 3,
         }
       },
     })
